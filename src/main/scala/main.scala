7af2063ea781049b188a9857c721653c5997efaa
arnautdj <s.frv86@gmail.com>
2025-01-24 17:14:19 -0500

    update

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,2 +1,29 @@
-object main {
+import cats.effect.{IO, IOApp}
+import kantan.csv._
+import kantan.csv.ops._
+import kantan.csv.generic._
+import model.Estudiante
+import java.io.File
+
+// Extiende de IOApp.Simple para manejar efectos IO y recursos de forma segura
+object Main extends IOApp.Simple {
+  val path2DataFile2 = "src/main/resources/data/estudiantes.csv"
+
+  val dataSource = new File(path2DataFile2)
+    .readCsv[List, Estudiante](rfc.withHeader.withCellSeparator(','))
+
+  val estudiante = dataSource.collect {
+    case Right(estudiante) => estudiante
+  }
+
+  /**
+   * Punto de entrada principal de la aplicación.
+   * Lee temperaturas desde CSV, las inserta en la base de datos,
+   * e imprime el número de registros insertados.
+   *
+   * @return IO[Unit] que representa la secuencia de operaciones
+   */
+  def run: IO[Unit] =
+    EstudianteDAO.insertAll(estudiante)
+      .flatMap(result => IO.println(s"Registros insertados: ${result.size}"))
 }
